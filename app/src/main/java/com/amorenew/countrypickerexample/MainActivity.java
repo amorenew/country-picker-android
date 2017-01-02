f2748f3f8b6e9e8b24607f708fd79ac17b04e779
amorenew <amorenew@gmail.com>
2017-01-02 16:55:48 +0300

    fix row color in light theme

---
+++ app/src/main/java/com/amorenew/countrypickerexample/MainActivity.java
@@ -0,0 +1,65 @@
+package com.amorenew.countrypickerexample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.amorenew.countrypicker.fragments.CountryPicker;
+import com.amorenew.countrypicker.interfaces.CountryPickerListener;
+import com.amorenew.countrypicker.models.Country;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mCountryNameTextView, mCountryIsoCodeTextView, mCountryDialCodeTextView;
+    private ImageView mCountryFlagImageView;
+    private Button mPickCountryButton;
+    private CountryPicker mCountryPicker;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initialize();
+        setListener();
+    }
+
+    private void setListener() {
+        mCountryPicker.setListener(new CountryPickerListener() {
+            @Override
+            public void onSelectCountry(Country country) {
+                mCountryNameTextView.setText(country.getName());
+                mCountryIsoCodeTextView.setText(country.getCode());
+                mCountryDialCodeTextView.setText(country.getDialCode());
+                mCountryFlagImageView.setImageResource(country.getFlag());
+                mCountryPicker.dismiss();
+            }
+        });
+        mPickCountryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCountryPicker.show(getSupportFragmentManager(), "COUNTRY_PICKER");
+            }
+        });
+        getUserCountryInfo();
+    }
+
+    private void initialize() {
+        mCountryNameTextView = (TextView) findViewById(R.id.selected_country_name_text_view);
+        mCountryIsoCodeTextView = (TextView) findViewById(R.id.selected_country_iso_text_view);
+        mCountryDialCodeTextView = (TextView) findViewById(R.id.selected_country_dial_code_text_view);
+        mPickCountryButton = (Button) findViewById(R.id.country_picker_button);
+        mCountryFlagImageView = (ImageView) findViewById(R.id.selected_country_flag_image_view);
+        mCountryPicker = CountryPicker.newInstance(getString(R.string.select_dial_code));
+    }
+
+    private void getUserCountryInfo() {
+        Country country = mCountryPicker.getUserCountryInfo(this);
+        mCountryFlagImageView.setImageResource(country.getFlag());
+        mCountryDialCodeTextView.setText(country.getDialCode());
+        mCountryIsoCodeTextView.setText(country.getCode());
+        mCountryNameTextView.setText(country.getName());
+    }
+}
